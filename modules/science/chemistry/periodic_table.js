14bd274f5905d0ee2e30c6bab1f342c58018a786
Caldeira-Edezio <103847048+Caldeira-Edezio@users.noreply.github.com>
2026-01-03 07:59:35 -0300

    Update periodic_table.js

--- modules/science/chemistry/periodic_table.js
+++ modules/science/chemistry/periodic_table.js
@@ -2,12 +2,12 @@
 <html>
   <head>
     <title>Site - Aurora das Histórias</title>
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/Caldeira-Edezio/aurora_das_historias@refs/heads/main/settings/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/Caldeira-Edezio/aurora_das_historias@0adcf5b932dffb162bf304bae846ee87e1c71075/settings/stylesheet.css">
   </head>
   <body>
     
     <h1 class="header mainHeader">Tabela periódica de elementos</h1>
-    <h2 id="header secHeader">Elementos mundanos</h2>
+    <h2 class="header secHeader">Elementos mundanos</h2>
     
     <button class="elementButton" onclick="printElement(this.textContent)">Hidrogênio</button>
     <button class="elementButton" onclick="printElement(this.textContent)">Hélio</button>
