d61a78cffda627e106404eadbb12d79d83e9fa5e
Caldeira-Edezio <103847048+Caldeira-Edezio@users.noreply.github.com>
2026-03-22 21:33:04 -0300

    Delete database directory

--- database/elementos.js
+++
@@ -1,3 +0,0 @@
-const data = '{"elementosMundanos"[{"numeroAtomico":1,"nome":"Hidrogênio","simboloAtomico":"H","massaMolar":1,"classe":"Ametal","pontoFusao":13.99,"pontoEvaporacao":20.27,"tipoMagnetico":"Diamagnético","tipoMagico":"Catalizador","radioatividade":"Não"},{"numeroAtomico":2,"nome":"Hélio","simboloAtomico":"He","massaMolar":4,"classe":"Gás Nobre","pontoFusao":0.00,"pontoEvaporacao":4.22,"tipoMagnetico":"Diamagnético","tipoMagico":"Resistor","radioatividade":"Não"},]}';
-const elementos = JSON.parse(data);
-export default elementos;
